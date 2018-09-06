4aba67d328c8c8d2e6c42fa3d037b23fd8d15011
mygit <f3366544f@163.com>
2018-09-06 09:15:44 +0800

    修改

--- src/com/itheima/demo/Test.java
+++ src/com/itheima/demo/Test.java
@@ -4,5 +4,9 @@ public class Test {
     public void tester(){
         System.out.println("测试git上传");
         System.out.println("再次测试");
+        System.out.println("再次测试");
+        System.out.println("再次测试");
+        System.out.println("再次测试");
+
     }
 }
